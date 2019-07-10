c3ccb5fdee7430a6ce96ecea55202d88e630fa7e
JustinAndrade <j.andrade.john@gmail.com>
2019-07-10 03:12:23 -0400

    modals changed

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,116 +3,59 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faReact, faJsSquare, faNode } from '@fortawesome/free-brands-svg-icons';
 
 import portImg from '../img/portImg.jpg';
+import MyConjugator from '../img/MyConjugator.png';
 
 const Portfolio = () => {
 	return (
 		<div className="portfolio">
 			<a name="portfolio" />
-			<img src={portImg} />
+			<img className="portImg" src={portImg} />
 			<div className="pageContainer">
 				<div className="port" />
 				<div className="portContainer">
 					<div className="backdrops">
 						<h2>My Recent Work</h2>
 						<div className="backdrop">
+							<img className="projectImg" src={MyConjugator} />
 							<h3>MyConjugator</h3>
-							<p>A spanish conjugator, that allows you to conjugate words.</p>
-							<div className="projectButtons">
-								<span>Live</span>
-								<span>
-									<FontAwesomeIcon icon={faGithub} />
-								</span>
+							<div className="description">
+								<p>A spanish conjugator, that allows you to conjugate words.</p>
+								<div className="projectButtons">
+									<a target="#" href="http://myconjugator.com/">
+										Live
+									</a>
+									<a target="#" href="https://github.com/conjugator/spanish-conjugator">
+										<FontAwesomeIcon icon={faGithub} />
+									</a>
+								</div>
 							</div>
 							<div id="projectTools">
 								<p>
 									Built with: <FontAwesomeIcon color="#5FD4F4" size="lg" icon={faReact} />{' '}
-									<FontAwesomeIcon icon={faJsSquare} color="#F0D91D" size="lg" />
+									<FontAwesomeIcon icon={faJsSquare} color="#F0D91D" size="lg" />{' '}
 									<FontAwesomeIcon icon={faNode} color="#87C000" size="lg" />
 								</p>
 							</div>
 						</div>
 						<div className="backdrop">
+							<img className="projectImg" src={MyConjugator} />
 							<h3>MyConjugator</h3>
-							<p>A spanish conjugator, that allows you to conjugate words.</p>
-							<div className="projectButtons">
-								<span>Live</span>
-								<span>
-									<FontAwesomeIcon icon={faGithub} />
-								</span>
+							<div className="description">
+								<p>A spanish conjugator, that allows you to conjugate words.</p>
+								<div className="projectButtons">
+									<a target="#" href="http://myconjugator.com/">
+										Live
+									</a>
+									<a target="#" href="https://github.com/conjugator/spanish-conjugator">
+										<FontAwesomeIcon icon={faGithub} />
+									</a>
+								</div>
 							</div>
 							<div id="projectTools">
 								<p>
 									Built with: <FontAwesomeIcon color="#5FD4F4" size="lg" icon={faReact} />{' '}
-									<FontAwesomeIcon icon={faJsSquare} size="lg" color="#F0D91D" />{' '}
-									<FontAwesomeIcon icon={faNode} size="lg" color="#87C000" />
-								</p>
-							</div>
-						</div>
-						<div className="backdrop">
-							<h3>MyConjugator</h3>
-							<p>A spanish conjugator, that allows you to conjugate words.</p>
-							<div className="projectButtons">
-								<span>Live</span>
-								<span>
-									<FontAwesomeIcon icon={faGithub} />
-								</span>
-							</div>
-							<div id="projectTools">
-								<p>
-									Built with: <FontAwesomeIcon color="#5FD4F4" size="lg" icon={faReact} />{' '}
-									<FontAwesomeIcon icon={faJsSquare} size="lg" color="#F0D91D" />{' '}
-									<FontAwesomeIcon icon={faNode} size="lg" color="#87C000" />
-								</p>
-							</div>
-						</div>
-						<div className="backdrop">
-							<h3>MyConjugator</h3>
-							<p>A spanish conjugator, that allows you to conjugate words.</p>
-							<div className="projectButtons">
-								<span>Live</span>
-								<span>
-									<FontAwesomeIcon icon={faGithub} />
-								</span>
-							</div>
-							<div id="projectTools">
-								<p>
-									Built with: <FontAwesomeIcon color="#5FD4F4" size="lg" icon={faReact} />{' '}
-									<FontAwesomeIcon icon={faJsSquare} size="lg" color="#F0D91D" />{' '}
-									<FontAwesomeIcon icon={faNode} size="lg" color="#87C000" />
-								</p>
-							</div>
-						</div>
-						<div className="backdrop">
-							<h3>MyConjugator</h3>
-							<p>A spanish conjugator, that allows you to conjugate words.</p>
-							<div className="projectButtons">
-								<span>Live</span>
-								<span>
-									<FontAwesomeIcon icon={faGithub} />
-								</span>
-							</div>
-							<div id="projectTools">
-								<p>
-									Built with: <FontAwesomeIcon color="#5FD4F4" size="lg" icon={faReact} />{' '}
-									<FontAwesomeIcon icon={faJsSquare} size="lg" color="#F0D91D" />{' '}
-									<FontAwesomeIcon icon={faNode} size="lg" color="#87C000" />
-								</p>
-							</div>
-						</div>
-						<div className="backdrop">
-							<h3>MyConjugator</h3>
-							<p>A spanish conjugator, that allows you to conjugate words.</p>
-							<div className="projectButtons">
-								<span>Live</span>
-								<span>
-									<FontAwesomeIcon icon={faGithub} />
-								</span>
-							</div>
-							<div id="projectTools">
-								<p>
-									Built with: <FontAwesomeIcon color="#5FD4F4" size="lg" icon={faReact} />{' '}
-									<FontAwesomeIcon icon={faJsSquare} size="lg" color="#F0D91D" />{' '}
-									<FontAwesomeIcon icon={faNode} size="lg" color="#87C000" />
+									<FontAwesomeIcon icon={faJsSquare} color="#F0D91D" size="lg" />{' '}
+									<FontAwesomeIcon icon={faNode} color="#87C000" size="lg" />
 								</p>
 							</div>
 						</div>
