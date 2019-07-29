bbda54fee6ad4746b528f52b26508596ec504b50
JustinAndrade <j.andrade.john@gmail.com>
2019-07-29 05:07:07 -0400

    home page complete

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -19,11 +19,15 @@ const Home = () => {
 					<button>Contact Me</button>
 				</div>
 				<div className="icons">
-					<FontAwesomeIcon className="github" icon={faGithub} size="6x" />
-
-					<FontAwesomeIcon className="linkedin" icon={faLinkedinIn} size="6x" />
-
-					<FontAwesomeIcon className="twitter" icon={faTwitter} size="6x" />
+					<a target="#" href="http://www.github.com/justinandrade">
+						<FontAwesomeIcon className="github" icon={faGithub} size="6x" />
+					</a>
+					<a target="#" href="http://www.linkedin.com/in/justin-andrade-75aab1150/">
+						<FontAwesomeIcon className="linkedin" icon={faLinkedinIn} size="6x" />
+					</a>
+					<a target="#" href="http://www.twitter.com/justinandrade_j">
+						<FontAwesomeIcon className="twitter" icon={faTwitter} size="6x" />
+					</a>
 				</div>
 			</div>
 		</div>
