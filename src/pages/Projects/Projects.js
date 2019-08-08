dc11eb85604f98d6fb0112d938b1bc29aaf11974
Justin <j.andrade.john@gmail.com>
2019-08-08 01:58:31 -0400

    description updated

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -15,16 +15,14 @@ const Projects = (props) => {
 				<div className="project1">
 					<h3>MyConjugator</h3>
 					<p>
-						A spanish conjugator, that allows you to conjugate words for studying purposes. This also
-						includes a leveling system. Built with React, Redux, Javascript.
+						<i>MyConjugator</i> is a spanish conjugator, that allows users to conjugate words for studying purposes.  This application also includes a leveling system for a fun learning experience.  Users are able to register and authentication requires JSON Web Tokens.  Built with React, Redux, and Javascript.
 					</p>
 					<div className="project-img" />
 				</div>
 				<div className="project2">
 					<h3>Bookr</h3>
 					<p>
-						A spanish conjugator, that allows you to conjugate words for studying purposes. This also
-						includes a leveling system. Built with React, Redux, Javascript.
+						Looking for your next book? Look no further! <i>Bookr</i> is a platform where users are able to search books leave reviews.  Created with React, Redux, Javascript, NodeJS, Jest.
 					</p>
 					<div className="project-img" />
 				</div>
@@ -39,8 +37,7 @@ const Projects = (props) => {
 				<div className="project4">
 					<h3>Justin's Notes</h3>
 					<p>
-						A spanish conjugator, that allows you to conjugate words for studying purposes. This also
-						includes a leveling system. Built with React, Redux, Javascript.
+						Simple and sleek note creating app that allows a user to track tasks as-well as mark tasks completed.  
 					</p>
 					<div className="project-img" />
 				</div>
